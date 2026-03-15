45e9b3c6122435f0c65335d98c78505c91027792
emavolant <volanti.1761743@studenti.uniroma1.it>
2026-03-15 17:25:23 +0100

    Use all the quarters

--- lib/controllers/exercise_controller.dart
+++ lib/controllers/exercise_controller.dart
@@ -123,7 +123,7 @@ class ExerciseController extends ChangeNotifier {
   }
 
   MeasureModel _generateRandomMeasure() {
-    const patterns = [8, 10, 12, 15, 0];
+    const patterns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
     return MeasureModel(
       List.generate(
